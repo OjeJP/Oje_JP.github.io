e626f360a52d13ebf426b5f12e29a000dcc64945
Oje Johnson Pamilerin <105719692+OjeJP@users.noreply.github.com>
2025-06-22 18:38:03 +0300

    Initial

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+@app.route('/certifications')
+def certifications():
+    return render_template("certifications.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/blog')
+def blog():
+    return render_template("blog.html")  # Optional
+
+if __name__ == "__main__":
+    app.run(debug=True)
